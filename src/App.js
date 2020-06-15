c95eadea3ccb68785bc26db902f620cd33bdc923
Axel <axelharyanto@outlook.com>
2020-06-15 20:32:33 +0700

    Cara Menggunakan React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import About from './Component/Fungsional/AboutComp';
+//import logo from './logo.svg';
+//import Home from './Component/Fungsional/Home';
+//import Parent from './Component/Class/Parent';
+//import BootstrapComp from './Component/Class/BootstrapComp';
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import HomePage from './Component/Fungsional/HomePage';
+//import './App.css';
 
-function App() {
+//import Beranda from './Component/Class/Beranda';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavbarComp />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
+
+
   );
 }
 
---
+++ src/Component/Fungsional/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+//import Blog from './Blog'
+import Produk from '../Class/Produk';
+
+const Home = () => {
+    return <div>
+        <Produk nama="Macbook Pro 2016" stock="10" harga="30000000"/>
+        <Produk nama="Macbook Pro 2017" stock="15" harga="31000000"/>
+        <Produk nama="Macbook Pro 2018" stock="20" harga="32000000"/>
+        <Produk nama="Macbook Pro 2019" stock="25" harga="33000000"/>
+        <Produk nama="Macbook Pro 2020" stock="30" harga="34000000"/>
+        <Produk nama="Macbook Pro 2021" stock="35" harga="35000000"/>
+    </div>
+}
+
+export default Home;

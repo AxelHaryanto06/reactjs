33c541974554059e3307dffd15153ec1e3b02d7d
Axel <axelharyanto@outlook.com>
2020-06-24 23:38:39 +0700

    React Hooks useState

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import DetailComp from './Component/Fungsional/DetailComp';
 import ListComp from './Component/Class/ListComp';
 import TambahComp from './Component/Class/TambahComp';
 import EditComp from './Component/Class/EditComp';
+import KelasComp from './Component/Hooks/Class/KelasComp';
+import HooksComp from './Component/Hooks/Functional/HooksComp';
 //import './App.css';
 
 //import Beranda from './Component/Class/Beranda';
@@ -25,6 +27,8 @@ const App = () => {
         <Route exact path="/mahasiswa" component={ListComp} />
         <Route exact path="/mahasiswa/tambah" component={TambahComp} />
         <Route exact path="/mahasiswa/edit" component={EditComp} />
+        <Route exact path="/kelas" component={KelasComp} />
+        <Route exact path="/hooks" component={HooksComp} />
         
         {/* <Route exact path="/detail/:id" component={DetailComp} /> */}
       </Switch>
